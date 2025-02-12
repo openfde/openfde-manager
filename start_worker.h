9a419bbb14b710e6f64875634e457fa508897243
185457686 <185457686@qq.com>
2025-02-12 16:49:10 +0800

    replace start with execute

--- start_worker.h
+++ start_worker.h
@@ -19,11 +19,12 @@ public slots:
     void doStartWork() {
 	    // 创建一个方法调用消息
         qDebug() << "Worker: 开始执行任务，线程 ID:" << QThread::currentThreadId();
-	    process = new QProcess();
-        process->start("/usr/bin/fde_utils", QStringList() << "start");
-        process->waitForFinished(-1);
-        QString output(process->readAllStandardOutput());
-        qDebug() << "fde_utils output:" << output;
+        int exitCode = QProcess::execute("/usr/bin/fde_utils", QStringList() << "start");
+        if (exitCode == 0) {
+            qDebug() << "fde_utils executed successfully";
+        } else {
+            qDebug() << "fde_utils execution failed with exit code:" << exitCode;
+        }
         emit startEnded(); // 发送任务完成信号
 	    delete process;
     }
