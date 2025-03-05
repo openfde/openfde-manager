5e6c8880170afa4ae32b6115befef7c1817f5668
gongyong <185457686@qq.com>
2025-03-05 18:49:02 +0800

    complete zh qm

--- src/translations/language_zh.ts
+++ src/translations/language_zh.ts
@@ -1,13 +1,167 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="en_US">
 <context>
     <name>MainWindow</name>
     <message>
-        <source>FDE dbus服务未启动</source>
-        <translation type="unfinished"></translation>
+        <source>FDE Dbus服务未启动</source>
+        <translation type="unfinished">FDE Dbus service not started</translation>
+    </message>
+    <message>
+        <source>网络或磁盘故障</source>
+        <translation type="unfinished">Network failure</translation>
+    </message>
+    <message>
         <source>设置</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Settings</translation>
+    </message>
+    <message>
+        <source>卸载</source>
+        <translation type="unfinished">Uninstall</translation>
+    </message>
+    <message>
+        <source>卸载时一起删除数据</source>
+        <translation type="unfinished">Clear user data</translation>
+    </message>
+    <message>
+        <source>系统设置</source>
+        <translation type="unfinished">System Settings</translation>
+    </message>
+    <message>
+        <source>OpenFDE版本</source>
+        <translation type="unfinished">OpenFDE Version</translation>
+    </message>
+    <message>
+        <source>控制程序版本</source>
+        <translation type="unfinished">Control Version</translation>
+    </message>
+    <message>
+        <source>OpenFDE未安装</source>
+        <translation type="unfinished">OpenFDE not installed</translation>
+    </message>
+    <message>
+        <source>确定要卸载OpenFDE吗？</source>
+        <translation type="unfinished">Confirm to uninstall OpenFDE?</translation>
+    </message>
+    <message>
+        <source>隐藏窗口</source>
+        <translation type="unfinished">Hidden Window</translation>
+    </message>
+    <message>
+        <source>确认卸载</source>
+        <translation type="unfinished">Confirm Uninstalling</translation>
+    </message>
+    <message>
+        <source>准备中...</source>
+        <translation type="unfinished">Preparing...</translation>
+    </message>
+    <message>
+        <source>下载中...</source>
+        <translation type="unfinished">Downloading...</translation>
+    </message>
+    <message>
+        <source>解压中...</source>
+        <translation type="unfinished">Extracting...</translation>
+    </message>
+    <message>
+        <source>安装中...</source>
+        <translation type="unfinished">Installing...</translation>
+    </message>
+    <message>
+        <source>取消</source>
+        <translation type="unfinished">Cancel</translation>
+    </message>
+    <message>
+        <source>启动OpenFDE</source>
+        <translation type="unfinished">Start OpenFDE</translation>
+    </message>
+    <message>
+        <source>不支持的GPU</source>
+        <translation type="unfinished">The GPU is not support</translation>
+    </message>
+    <message>
+        <source>不支持的内核</source>
+        <translation type="unfinished">The Kernel is not support</translation>
+    </message>
+    <message>
+        <source>系统错误</source>
+        <translation type="unfinished">System Error</translation>
+    </message>
+    <message>
+        <source>系统环境错误</source>
+        <translation type="unfinished">System Error</translation>
+    </message>
+    <message>
+        <source>您选择退出安装</source>
+        <translation type="unfinished">You choosed to exit</translation>
+    </message>
+    <message>
+        <source>安装Binder失败</source>
+        <translation type="unfinished">Install Binder failed</translation>
+    </message>
+    <message>
+        <source>不支持的参数</source>
+        <translation type="unfinished">Unsupport args</translation>
+    </message>
+    <message>
+        <source>请先关闭OpenFDE</source>
+        <translation type="unfinished">Please stop OpenFDE first</translation>
+    </message>
+    <message>
+        <source>未安装curl命令</source>
+        <translation type="unfinished">Please install curl</translation>
+    </message>
+    <message>
+        <source>Pid Max超过65535</source>
+        <translation type="unfinished">Pid Max great than 65535</translation>
+    </message>
+    <message>
+        <source>网络错误</source>
+        <translation type="unfinished">Network error</translation>
+    </message>
+    <message>
+        <source>apt源错误</source>
+        <translation type="unfinished">apt failure</translation>
+    </message>
+    <message>
+        <source>已取消</source>
+        <translation type="unfinished">Canceled</translation>
+    </message>
+    <message>
+        <source>确定要取消下载吗?</source>
+        <translation type="unfinished">Are you sure to cancel the downloading?</translation>
+    </message>
+    <message>
+        <source>卸载成功</source>
+        <translation type="unfinished">Uninstalled </translation>
+    </message>
+    <message>
+        <source>安装完成</source>
+        <translation type="unfinished">Installed </translation>
+    </message>
+    <message>
+        <source>需要关闭应用执行控制才能继续</source>
+        <translation type="unfinished">Need turn off the application exec controling</translation>
+    </message>
+    <message>
+        <source>未安装</source>
+        <translation type="unfinished">UnInstalled </translation>
+    </message>
+</context>
+
+<context>
+    <name>QObject</name>
+    <message>
+        <source>恢复窗口</source>
+        <translation type="unfinished">Restore window</translation>
+    </message>
+    <message>
+        <source>最小化</source>
+        <translation type="unfinished">Minimize</translation>
+    </message>
+    <message>
+        <source>退出</source>
+        <translation type="unfinished">Quit</translation>
     </message>
 </context>
 </TS>
